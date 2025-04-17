bf93adc6014adb3e77acd59251b9ab5d0be9791b
xSaTaNxCreWx <soverload@mail.ru>
2025-04-17 18:57:43 +0800

    Updates

--- assets/main-BCNTRSxH.js
+++ assets/main-BCNTRSxH.js
@@ -5596,6 +5596,25 @@ if (side && gallery) {
   });
   resizeObserver.observe(side);
 }
+const tabsBtn = document.querySelectorAll(".store-locator__tab");
+const tabs = document.querySelectorAll(".store-locator__tabs-item");
+tabsBtn.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    tabs.forEach((tab, tabIndex) => {
+      tab.classList.toggle("store-locator__tabs-item--active", tabIndex === index);
+    });
+    tabsBtn.forEach((button, btnIndex) => {
+      button.classList.toggle("store-locator__tab--active", btnIndex === index);
+    });
+  });
+});
+const locatorItems = document.querySelectorAll(".store-locator__item-header");
+locatorItems.forEach((target) => {
+  target.addEventListener("click", function() {
+    const item = this.closest(".store-locator__item");
+    item.classList.toggle("store-locator__item--active");
+  });
+});
 const sliders$1 = document.querySelectorAll(".base-slider");
 if (sliders$1.length) {
   sliders$1.forEach((slider2) => {
